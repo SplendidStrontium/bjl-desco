a18cb6d4d6fb0d9d8d421feb4d7eee02912d1313
Paige <pnkrsnak@gmail.com>
2015-06-05 18:18:05 -0400

    drew a bolt head

--- app/assets/javascripts/canvas_logo.js
+++ app/assets/javascripts/canvas_logo.js
@@ -8,14 +8,50 @@ function draw(){
   if (canvas.getContext){
     var ctx = canvas.getContext('2d');
 
-    ctx.fillStyle = "rgb(200,0,0)";
-    ctx.fillRect (10, 10, 55, 50);
+    /*draw the bolt top for underline*/
+    
+      ctx.fillStyle = '#778899'; /*hsb 210 22 60*/
+      ctx.beginPath();
+      ctx.moveTo(20,120);
+      ctx.lineTo(24,114);
+      ctx.lineTo(32,114);
+      ctx.lineTo(36,120);
+      ctx.lineTo(32,126);
+      ctx.lineTo(24,126);
+      ctx.fill();
 
-    ctx.fillStyle = "rgba(0,0,200,0.5)";
-    ctx.fillRect (30, 30, 55, 50);
+      ctx.fillStyle = '#637180'; /*left*/
+      ctx.beginPath();
+      ctx.moveTo(20,120);
+      ctx.lineTo(24,126);
+      ctx.lineTo(24,131);
+      ctx.lineTo(20,125);
+      ctx.fill();
 
-    ctx.font = "30px serif";
-    ctx.fillStyle = 'black';
-    ctx.fillText("B.J. Laura & Son", 20, 120);
+    ctx.fillStyle = '#95aabf'; /*middle*/
+    ctx.beginPath();
+    ctx.moveTo(24,126);
+    ctx.lineTo(32,126);
+    ctx.lineTo(32,131);
+    ctx.lineTo(24,131);
+    ctx.fill();
+
+    ctx.fillStyle = '#3c444d'; /*right*/
+    ctx.beginPath();
+    ctx.moveTo(32,126);
+    ctx.lineTo(36,120);
+    ctx.lineTo(36,125);
+    ctx.lineTo(32,131);
+    ctx.fill();
+
+    /*text bj laura*/
+    drw_title(ctx);
   }
+}
+
+/* draw title text */
+function drw_title(context){
+  context.font = "30px serif";
+  context.fillStyle = 'black';
+  context.fillText("B.J. Laura & Son", 20, 70);
 }
