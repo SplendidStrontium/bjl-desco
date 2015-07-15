c1e93793b960924789f1bcc1947c7f35bc8d7257
Paige <pnkrsnak@gmail.com>
2015-07-15 13:24:36 -0400

    additional validations for user fields

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150714184512) do
+ActiveRecord::Schema.define(version: 20150715171304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,5 +22,7 @@ ActiveRecord::Schema.define(version: 20150714184512) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
