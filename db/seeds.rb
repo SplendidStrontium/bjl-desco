bcebb36f8f3c782c2817f348723ffc56b8853c10
Paige Krsnak <pnkrsnak@gmail.com>
2021-01-20 12:48:54 -0500

    clean dir

--- db/seeds.rb
+++
@@ -1,9 +0,0 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-admin = User.create(name: "Paige Krsnak", email: "pnk@descoservice.com", password: "8admin0", password_confirmation: "8admin0")
